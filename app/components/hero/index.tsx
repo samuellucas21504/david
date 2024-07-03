13eebbc289d4670b1321057e6b2faebe12aed39e
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-07-03 15:54:20 -0300

    tes

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -8,7 +8,6 @@ import { ThemeSwitch } from "../theme_switch";
 import { useTheme } from "next-themes";
 import { Themes } from "../themes";
 import Image from "next/image";
-import { prefix } from "../../utils/prefix";
 
 interface HeroProps {
     handleLoad: () => void;
@@ -48,7 +47,7 @@ export default function Hero({handleLoad}: HeroProps) {
                 </Canvas>
             </div>
             <div className="spin">
-                <Image src={`${prefix}/rotate.png`} width={50} height={25} alt="Rotate symbol" />
+                <Image src={`/rotate.png`} width={50} height={25} alt="Rotate symbol" />
             </div>
 
 
